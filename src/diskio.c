1c2aaed7ebd88bffc9e09698f34d3dac132bee70
drdanick <di.da.nick@gmail.com>
2017-03-06 20:21:34 -0800

    Implemented disk io.

--- src/diskio.c
+++ src/diskio.c
@@ -1,1 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "diskio.h"
+
+char* loadDataFromDiskFile(char* fileName) {
+    FILE* diskFile = fopen(fileName, "rb");
+    char* diskData = (char*)malloc(sizeof(char) * DISK_SIZE);
+
+    if(!fgets(diskData, DISK_SIZE, diskFile)) {
+        if(diskFile)
+            fclose(diskFile);
+        free(diskData);
+        return NULL;
+    }
+
+    fclose(diskFile);
+    return diskData;
+}
+
+int saveDiskFile(char* fileName, char* data) {
+    FILE* diskFile = fopen(fileName, "wb");
+
+    if(!diskFile)
+        return -1;
+
+    if(!fwrite(data, sizeof(char), DISK_SIZE, diskFile)) {
+        fclose(diskFile);
+        return -1;
+    }
+
+    fclose(diskFile);
+    return 1;
+}
+
+void freeDiskData(char* diskData) {
+    free(diskData);
+}
+
+char* getSegmentData(char* data, int tracknum, int sectornum) {
+    return &data[
+        ((tracknum & TRACK_ID_MASK) * TRACK_SIZE) +
+        ((sectornum & SECTOR_ID_MASK) * SECTOR_SIZE)
+    ];
+}
--- src/diskio.h
+++ src/diskio.h
@@ -1,6 +1,8 @@
 #ifndef DISKIO_H
 #define DISKIO_H
 
+#include <stdio.h>
+
 /*
  * Disk geometry constants
  */
@@ -8,5 +10,55 @@
 #define SECTORS_PER_TRACK 64
 #define SECTOR_SIZE       256
 #define TRACK_SIZE        (SECTORS_PER_TRACK * SECTOR_SIZE)
+#define DISK_SIZE         (TRACKS * SECTORS_PER_TRACK * SECTOR_SIZE)
+
+/*
+ * Masks to prevent IDs from being out of range
+ */
+#define TRACK_ID_MASK  0x1F
+#define SECTOR_ID_MASK 0x3F
+
+/*
+ * Functions
+ */
+
+/**
+ * Load and cache data from a disk image file.
+ *
+ * fileName: The name/path of the disk image file to load
+ *
+ * Returns: A pointer to the loaded data.
+ */
+char* loadDataFromDiskFile(char* fileName);
+
+/**
+ * Save disk image data to a file
+ * 
+ * fileName: The name/path of the file to save to.
+ * data:     A pointer to the data to save.
+ *
+ * Returns: 1 if the operation was successful, -1 otherwise.
+ *
+ */
+int saveDiskFile(char* fileName, char* data);
+
+/**
+ * Free cached disk data
+ *
+ * diskData: A pointer to the disk data cache.
+ */
+void freeDiskData(char* diskData);
+
+/**
+ * Given cached disk data, retrieve a pointer to a specific disk sector.
+ *
+ * data:      A pointer to the disk data cache.
+ * tracknum:  The track number to load.
+ * sectornum: The sector number to load.
+ *
+ * Returns: A pointer to the requested segment within the cache.
+ * Note that no data is copied.
+ */
+char* getSegmentData(char* data, int tracknum, int sectornum);
 
 #endif /* DISKIO_H */
